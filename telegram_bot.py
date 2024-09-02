ef19201175d595c9bb543499624e4ddcaafc8de5
nonegom <fksl9959@naver.com>
2024-09-02 18:35:32 +0900

    feat: make good word

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,6 +4,8 @@ from datetime import datetime
 import os 
 from dotenv import load_dotenv
 import json
+import random
+
 load_dotenv(verbose=True)
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
@@ -20,7 +22,7 @@ class telegramBot():
     def load_good_word_list(self):
         with open('good_word_list.json', 'r', encoding='utf-8') as file:
             word_list = json.load(file)
-        return word_list
+        return word_list['yourRgood']
 
     async def checkbot(self):
         print(await self.bot.get_me())
@@ -30,8 +32,9 @@ class telegramBot():
         chat_id = updates[0]['message']['chat']['id']
         return chat_id
 
-    async def main(self, message):
-        await self.bot.send_message(text=f'{current_time}에도 너는 아주 잘하고 있어! 최고야!!', chat_id=self.chat_id)
+    async def main(self, text):
+        good_word4you = random.choice(self.good_word_list)
+        await self.bot.send_message(text=f'{current_time} / {good_word4you}', chat_id=self.chat_id)
         print('message send!')
 
 if __name__ == '__main__':
