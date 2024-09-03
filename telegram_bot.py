c80d1516b39eb74a54934a44dc03cf4fe7abbecf
nonegom <fksl9959@naver.com>
2024-09-03 18:36:28 +0900

    feat: good_word_list 추가 및 json 로드 코드 작성

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,6 +2,7 @@ import asyncio
 import telegram
 from datetime import datetime
 import os 
+import json
 from dotenv import load_dotenv
 load_dotenv(verbose=True)
 
@@ -13,6 +14,12 @@ class telegramBot():
     def __init__(self):
         self.bot = telegram.Bot(token=BOT_TOKEN)
         self.chat_id = asyncio.run(self.load_chat_id())
+        self.good_word_list = self.load_good_word_list()             
+
+    def load_good_word_list(self):
+        with open('good_word_list.json', 'r', encoding='utf-8') as file:
+            word_list = json.load(file)
+        return word_list
 
     async def checkbot(self):
         print(await self.bot.get_me())
@@ -21,7 +28,6 @@ class telegramBot():
         updates = (await self.bot.get_updates())
         chat_id = updates[0]['message']['chat']['id']
         return chat_id
-
 
     async def main(self, message):
         await self.bot.send_message(text=f'{current_time}에도 너는 아주 잘하고 있어! 최고야!!', chat_id=self.chat_id)
